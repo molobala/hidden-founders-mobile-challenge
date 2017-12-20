6ff41513e0da28b008e0c1670e16a5ab1479dfe1
molobala <doumbiamahamadou.ensate@gmail.com>
2017-12-20 22:15:40 +0000

    Change the application design pattern * Using fragment in the main activity * Add TrendingFragment wich will next contain the repository   list * Change MainActivity to adapt it to fragments
    Signed-off-by: molobala <doumbiamahamadou.ensate@gmail.com>

--- app/src/main/java/com/molo/app/challenge/mobile/MainActivity.java
+++ app/src/main/java/com/molo/app/challenge/mobile/MainActivity.java
@@ -5,23 +5,22 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import com.molo.app.challenge.mobile.fragments.TrendingFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            android.support.v4.app.FragmentManager fragmentManager=getSupportFragmentManager();
+            android.support.v4.app.FragmentTransaction transaction=fragmentManager.beginTransaction();
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_star);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_settings);
+                case R.id.trend:
+                    transaction.add(R.id.fragment,TrendingFragment.newInstance()).commit();
                     return true;
             }
             return false;
@@ -33,8 +32,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment,TrendingFragment.newInstance()).commit();
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
---
+++ app/src/main/java/com/molo/app/challenge/mobile/fragments/TrendingFragment.java
@@ -0,0 +1,93 @@
+package com.molo.app.challenge.mobile.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.ListView;
+import com.molo.app.challenge.mobile.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link TrendingFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link TrendingFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TrendingFragment extends Fragment {
+    private ListView repositoryList;
+    private OnFragmentInteractionListener mListener;
+
+    public TrendingFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment TrendingFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static TrendingFragment newInstance() {
+        TrendingFragment fragment = new TrendingFragment();
+        Bundle args = new Bundle();
+        //add initialisation params here if any
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+           //get initialisation args
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v= inflater.inflate(R.layout.fragment_trending, container, false);
+        repositoryList=v.findViewById(R.id.repository_list);
+        return v;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            /*throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");*/
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+
+    }
+}
