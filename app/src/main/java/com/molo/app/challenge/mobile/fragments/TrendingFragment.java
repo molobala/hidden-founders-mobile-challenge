98095c7a6f15285bfd793a4bd77a4ebdb6e3f281
molobala <doumbiamahamadou.ensate@gmail.com>
2017-12-21 02:51:59 +0000

    Add github server call in the TrendingFragment *  Make TrendingFragment implementing HttpResponseListener so    we could populate the listView on request response from github
    Signed-off-by: molobala <doumbiamahamadou.ensate@gmail.com>

--- app/src/main/java/com/molo/app/challenge/mobile/fragments/TrendingFragment.java
+++ app/src/main/java/com/molo/app/challenge/mobile/fragments/TrendingFragment.java
@@ -10,11 +10,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.ListView;
+import com.android.volley.toolbox.StringRequest;
 import com.molo.app.challenge.mobile.R;
 import com.molo.app.challenge.mobile.adapters.RepositoryListAdapter;
 import com.molo.app.challenge.mobile.models.Owner;
 import com.molo.app.challenge.mobile.models.Repository;
 import com.molo.app.challenge.mobile.utils.Utils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,11 +28,12 @@ import com.molo.app.challenge.mobile.utils.Utils;
  * Use the {@link TrendingFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TrendingFragment extends Fragment {
+public class TrendingFragment extends Fragment implements Utils.HttpResultListener {
     private RepositoryListAdapter repositoryListAdapter;
     private ListView repositoryList;
     private OnFragmentInteractionListener mListener;
-
+    private StringRequest request;
+    private static final String REPOSITORY_URL="https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc";
     public TrendingFragment() {
         // Required empty public constructor
     }
@@ -62,16 +67,17 @@ public class TrendingFragment extends Fragment {
         View v= inflater.inflate(R.layout.fragment_trending, container, false);
         repositoryList=v.findViewById(R.id.repository_list);
         repositoryListAdapter=new RepositoryListAdapter(getContext());
-        //for test case
-        repositoryListAdapter.add(new Repository(12,"molo-c-utils","une libraire ecrite en c",2452,new Owner("molobala","https://avatars2.githubusercontent.com/u/1728152?v=4",1)));
-        repositoryListAdapter.add(new Repository(12,"molo-c-utils","une libraire ecrite en c",2452,new Owner("molobala","https://avatars2.githubusercontent.com/u/1728152?v=4",1)));
-        repositoryListAdapter.add(new Repository(12,"molo-c-utils","une libraire ecrite en c",2452,new Owner("molobala","https://avatars2.githubusercontent.com/u/1728152?v=4",1)));
-        repositoryListAdapter.add(new Repository(12,"molo-c-utils","une libraire ecrite en c",2452,new Owner("molobala","https://avatars2.githubusercontent.com/u/1728152?v=4",1)));
-        repositoryListAdapter.add(new Repository(12,"molo-c-utils","une libraire ecrite en c",2452,new Owner("molobala","https://avatars2.githubusercontent.com/u/1728152?v=4",1)));
-        repositoryListAdapter.add(new Repository(12,"molo-c-utils","une libraire ecrite en c",2452,new Owner("molobala","https://avatars2.githubusercontent.com/u/1728152?v=4",1)));
         repositoryList.setAdapter(repositoryListAdapter);
+        //for test case
+       /* repositoryListAdapter.add(new Repository(12,"molo-c-utils","une libraire ecrite en c",2452,new Owner("molobala","https://avatars2.githubusercontent.com/u/1728152?v=4",1)));
+        repositoryListAdapter.add(new Repository(12,"molo-c-utils","une libraire ecrite en c",2452,new Owner("molobala","https://avatars2.githubusercontent.com/u/1728152?v=4",1)));
+        repositoryListAdapter.add(new Repository(12,"molo-c-utils","une libraire ecrite en c",2452,new Owner("molobala","https://avatars2.githubusercontent.com/u/1728152?v=4",1)));
+        repositoryListAdapter.add(new Repository(12,"molo-c-utils","une libraire ecrite en c",2452,new Owner("molobala","https://avatars2.githubusercontent.com/u/1728152?v=4",1)));
+        repositoryListAdapter.add(new Repository(12,"molo-c-utils","une libraire ecrite en c",2452,new Owner("molobala","https://avatars2.githubusercontent.com/u/1728152?v=4",1)));
+        repositoryListAdapter.add(new Repository(12,"molo-c-utils","une libraire ecrite en c",2452,new Owner("molobala","https://avatars2.githubusercontent.com/u/1728152?v=4",1)));
+        */
         //test of http method
-        Utils.http(getContext(), "https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc", new Utils.HttpResultAdapter() {
+        /*Utils.http(getContext(), "https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc", new Utils.HttpResultAdapter() {
             @Override
             public void onSuccess(String jsonString) {
                 super.onSuccess(jsonString);
@@ -83,7 +89,8 @@ public class TrendingFragment extends Fragment {
                 super.onFail(err);
                 Log.e("TrendingFrag.CreateV",err);
             }
-        });
+        });*/
+        request=Utils.http(getContext(),REPOSITORY_URL,this);
         return v;
     }
 
@@ -103,6 +110,33 @@ public class TrendingFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onSuccess(String jsonString) {
+        Log.e("TrendinFrag.onSucc",jsonString);
+        //parse the json string into an array of repositories
+        try {
+            JSONArray array= new JSONObject(jsonString).getJSONArray("items");
+            for(int i=0;i<array.length();i++){
+                JSONObject o=array.getJSONObject(i);
+                //create new repo
+                Repository rep=new Repository(o.getInt("id"),o.getString("name"),o.getString("description"),
+                        o.getInt("stargazers_count"),null);
+                o=o.getJSONObject("owner");
+                Owner owner=new Owner(o.getString("login"),o.getString("avatar_url"),o.getInt("id"));
+                rep.setOwner(owner);
+                //add the repo to the adapter
+                repositoryListAdapter.add(rep);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onFail(String err) {
+        Log.e("TrendinFrag.onSucc",err);
     }
 
     /**
